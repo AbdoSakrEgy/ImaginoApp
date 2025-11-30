fb38beacfc28bcf403fc72601c8d49346e3c5d46
Abdulrahim <abdulrahimsakr01@gmail.com>
2025-11-30 17:33:47 +0200

    add auth.plans.middleware

---
+++ dist/middlewares/auth.plans.middleware.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authPlans = void 0;
+const authPlans = (plansAllowed = []) => {
+    return async (req, res, next) => {
+    };
+};
+exports.authPlans = authPlans;
