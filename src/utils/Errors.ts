bf92b7de031521499c64b9dcb034fa904bb4ebbe
Abdulrahim <abdulrahimsakr01@gmail.com>
2025-12-04 21:44:43 +0200

    test github confilct

--- src/utils/Errors.ts
+++ src/utils/Errors.ts
@@ -18,7 +18,7 @@ export class ValidationError extends ApplicationException {
 }
 
 export class NotValidEmail extends ApplicationException {
-  constructor(msg: string = "Not valid email", statusCode: number = 400) {
+  constructor(msg: string = "Not validation email, plz valid it", statusCode: number = 400) {
     super(msg, statusCode);
   }
 }
