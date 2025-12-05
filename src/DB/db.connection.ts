c09d1cbc9c2ce9cd4e8563381933a9b2458b74f8
Abdulrahim <abdulrahimsakr01@gmail.com>
2025-12-05 16:02:42 +0200

    fix deploy

--- src/DB/db.connection.ts
+++ src/DB/db.connection.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  // if (!process.env.MONGODB_ATLAS_URL) {
-  //   throw new Error("MONGODB_ATLAS_URL is missing!");
-  // }
+  if (!process.env.MONGODB_ATLAS_URL) {
+    throw new Error("MONGODB_ATLAS_URL is missing!");
+  }
   await mongoose
-    .connect("mongodb+srv://abdosakr:abdo.sakr@cluster0.1taiy.mongodb.net/ImaginoApp")
+    .connect(process.env.MONGODB_ATLAS_URL as string)
     .then(() => {
       console.log("DB connected successfully");
     })
