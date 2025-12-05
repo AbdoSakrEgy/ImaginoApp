5dbc9b90246ae4227c6ce279d3896bbd3b225529
Abdulrahim <abdulrahimsakr01@gmail.com>
2025-12-05 15:20:59 +0200

    organize

--- src/DB/db.connection.ts
+++ src/DB/db.connection.ts
@@ -4,21 +4,12 @@ export const connectDB = async () => {
   if (!process.env.MONGODB_ATLAS_URL) {
     throw new Error("MONGODB_ATLAS_URL is missing!");
   }
-
-  // Set connection timeout
-  mongoose.set("serverSelectionTimeoutMS", 5000);
-  mongoose.set("socketTimeoutMS", 45000);
-
   await mongoose
-    .connect(process.env.MONGODB_ATLAS_URL, {
-      maxPoolSize: 10,
-      minPoolSize: 2,
-    })
+    .connect(process.env.MONGODB_ATLAS_URL as string)
     .then(() => {
       console.log("DB connected successfully");
     })
     .catch((err) => {
-      console.error("DB connection error:", err);
-      throw err;
+      console.log(err);
     });
 };
